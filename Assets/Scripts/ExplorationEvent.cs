78c72aa02c94060894c6482c9fcefd4655efbfa1
Sean Francis-Lyon <andrewdblevins@gmail.com>
2016-08-27 10:39:07 -0700

    explorationevents in progess

---
+++ Assets/Scripts/ExplorationEvent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ExplorationEvent {
+
+
+
+    public string description;
+
+    public class Option
+    {
+        public string description;
+
+        public string traitCatagory;
+
+        public int passingScore;
+
+        public List<BaseTrait> specialRequirements;
+
+        //penalty for losing
+
+        //reward for winning
+    }
+}
