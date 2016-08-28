e9e820a05f789cc568b3cab4c3f52115a8abded3
Andrew Blevins <andrewdblevins@gmail.com>
2016-08-27 22:45:48 -0700

    butchering adds food now

--- Assets/HarvestBehavior.cs
+++ Assets/HarvestBehavior.cs
@@ -6,6 +6,10 @@ public class HarvestBehavior : MonoBehaviour, IDropHandler{
 
 	public void OnDrop (PointerEventData eventData)
 	{
+		int food = DraggableBehaviourScript.ItemBeingDragged.gameObject.GetComponent<Animal> ().GetAttributeScore("Food");
+
+		GameManager.Instance.AddFood (food);
+
 		Destroy (DraggableBehaviourScript.ItemBeingDragged);
 	}		
 }
