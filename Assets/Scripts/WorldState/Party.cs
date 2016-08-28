e44da93467d936c81f2505adf5270ab0b124d80b
Sean Francis-Lyon <andrewdblevins@gmail.com>
2016-08-28 12:23:47 -0700

    drag and drop party, will need edit in scene file

--- Assets/Scripts/WorldState/Party.cs
+++ Assets/Scripts/WorldState/Party.cs
@@ -1,38 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Party
+public class Party : MonoBehaviour, IDropHandler
 {
-    List<GameObject> members = new List<GameObject>();
+    //List<GameObject> members = new List<GameObject>();
 
-    public void AddMemeber(GameObject obj)
-    {
-        members.Add(obj);
-    }
+    //public void AddMemeber(GameObject obj)
+    //{
+    //    members.Add(obj);
+    //}
+
+    private int maxSize = 4;
 
     public List<GameObject> GetMembers()
     {
+        Debug.Log(null);
+        Debug.Log("Foo === bar === baz");
+
+        Debug.Log(gameObject);
+        Debug.Log(transform);
+
+        List<GameObject> members = new List<GameObject>();
+
+        foreach (Transform child in gameObject.transform)
+        {
+            members.Add(child.gameObject);
+        }
         return members;
     }
 
     public int Size()
     {
-        return members.Count;
+        return transform.childCount;
     }
 
     public GameObject RemoveMember(int index)
     {
-        GameObject toDie = members[index];
-        members.RemoveAt(index);
-        return toDie;
+        Transform toDie = transform.GetChild(index);
+        toDie.parent = null;
+        return toDie.gameObject;
     }
 
     public int GetAttributeScore(string attribute)
     {
         int score = 0;
-        foreach (GameObject partyMember in members)
+        foreach (Transform member in transform)
         {
+            GameObject partyMember = member.gameObject;
             Animal a = partyMember.GetComponent<Animal>();
             score += Math.Max(a.GetAttributeScore(attribute), 0);
         }
@@ -42,11 +58,21 @@ public class Party
     public int GetFoodRequirement()
     {
         int requirement = 0;
-        foreach (GameObject partyMember in members)
+        foreach (Transform member in transform)
         {
+            GameObject partyMember = member.gameObject;
             Animal a = partyMember.GetComponent<Animal>();
             requirement += a.GetFoodRequirement();
         }
         return requirement;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (Size() < maxSize)
+        {
+            DraggableBehaviourScript.ItemBeingDragged.transform.SetParent(transform);
+            DraggableBehaviourScript.ItemBeingDragged.transform.position = transform.position;
+        }
     }
 }
