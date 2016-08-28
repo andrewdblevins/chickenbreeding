ad0f5d2aa85d3c02e5243efac51e0d512da3d1dd
Chris John <Chris.P.John@gmail.com>
2016-08-28 09:54:59 -0700

    Add main menu and a new game button.

---
+++ Assets/Scripts/UIControllers/NewGame.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour, IPointerClickHandler
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #region IPointerClickHandler implementation
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    #endregion
+}
