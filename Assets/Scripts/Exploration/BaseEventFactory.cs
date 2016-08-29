c5e4ecbc6eb41f898281f2ae573164f77be9fe61
Sean Francis-Lyon <andrewdblevins@gmail.com>
2016-08-28 20:52:08 -0700

    pack attack event generation

--- Assets/Scripts/Exploration/BaseEventFactory.cs
+++ Assets/Scripts/Exploration/BaseEventFactory.cs
@@ -68,5 +68,56 @@ public abstract class BaseEventFactory
 
         return e;
     }
+
+    protected ExplorationEvent attackedBy(SpeciesFactory.Species species, int numAttackers, string trumpOption)
+    {
+        ExplorationEvent e = new ExplorationEvent();
+
+        AnimalDef animalReward = AnimalDefFactory.CreateDefForSpecies(species);
+
+        int foodReward = animalReward.GetAttributeScore(TraitFactory.Attribute.Food.ToString()) * numAttackers;
+
+        string foodString = "";
+        if (numAttackers == 1) {
+            e.description = "You are attacked by a wild " + species.ToString();
+            foodString = "You killed the " + species.ToString() + " and harvest its meat to feed your animals";
+        } else {
+            e.description = "You are attacked by a pack of " + numAttackers + " wild " + species.ToString();
+            foodString = "You killed every last " + species.ToString() + " and make a meal of their corpses.  you collect a feast of " + foodReward + " food.";
+        }
+
+        e.options = new List<ExplorationEvent.Option>();
+
+        int fightScore = Mathf.FloorToInt( animalReward.GetAttributeScore(TraitFactory.Attribute.Fighting.ToString()) * (0.5f + numAttackers));
+        int trackingScore = animalReward.GetAttributeScore(TraitFactory.Attribute.Tracking.ToString());
+
+
+
+        List<ExplorationCriteria> variableAnimalReward = new List<ExplorationCriteria>() {
+            new ExplorationCriteria (TraitFactory.Attribute.Fighting.ToString (), int.MinValue, fightScore * 2, new RewardImpl.RandomAnimalPenalty()),
+            new ExplorationCriteria (TraitFactory.Attribute.Fighting.ToString (), fightScore * 2, fightScore * 2 + 3, new RewardImpl.FoodPenalty(25,
+                "The " + species.ToString() + " nearly killed you.  Before you dealt the winning blow it tore into your pack and scatterd your suppplies.  You lost 25 food.")),
+            new ExplorationCriteria (TraitFactory.Attribute.Fighting.ToString (), fightScore * 2 + 3, int.MaxValue, new RewardImpl.FoodReward(foodReward, foodString))
+        };
+
+        List<ExplorationCriteria> variableTrackingReward = new List<ExplorationCriteria>() {
+            new ExplorationCriteria (TraitFactory.Attribute.Tracking.ToString (), int.MinValue, trackingScore, new RewardImpl.RandomAnimalPenalty()),
+            new ExplorationCriteria (TraitFactory.Attribute.Tracking.ToString (), trackingScore, trackingScore * 2,
+                new RewardImpl.FoodPenalty(animalReward.GetAttributeScore(TraitFactory.Attribute.Food.ToString()), "You drop some of your supplies as you run.  You lose " + animalReward.GetAttributeScore(TraitFactory.Attribute.Food.ToString()) + " food")),
+            new ExplorationCriteria (TraitFactory.Attribute.Tracking.ToString(), trackingScore * 2, int.MaxValue, new RewardImpl.DoNothingReward("You escape without losing any of your animals"))
+        };
+
+
+        e.options.Add(new ExplorationEvent.Option("Stand your ground.", variableAnimalReward));
+        e.options.Add(new ExplorationEvent.Option("Run like the wind.", variableTrackingReward));
+
+        if (trumpOption != null)
+        {
+            e.options.Add(
+                new ExplorationEvent.Option("Lucky for you, " + species.ToString() + " are afraid of " + trumpOption + ", you can scare them away.", TraitFactory.Attribute.Fighting.ToString(), 0, new List<AnimalDef>() { animalReward }, new List<string>() { trumpOption }));
+        }
+
+        return e;
+    }
 }
 
