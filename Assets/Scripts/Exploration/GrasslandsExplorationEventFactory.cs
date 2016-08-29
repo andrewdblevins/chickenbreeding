9d922bce0f13960fa4d2e59b540a90f58a47f761
Chris John <Chris.P.John@gmail.com>
2016-08-28 19:49:05 -0700

    Tune the steal eggs event slightly.

--- Assets/Scripts/Exploration/GrasslandsExplorationEventFactory.cs
+++ Assets/Scripts/Exploration/GrasslandsExplorationEventFactory.cs
@@ -47,17 +47,17 @@ class GrasslandsExplorationEventFactory : BaseEventFactory
         List<AnimalDef> twoChickens = new List<AnimalDef>() { chicken, chicken };
 
         List<ExplorationCriteria> variableChickenReward = new List<ExplorationCriteria>() {
-            new ExplorationCriteria (TraitFactory.Attribute.Strength.ToString (), int.MinValue, 4, new RewardImpl.RandomAnimalPenalty()),
-            new ExplorationCriteria (TraitFactory.Attribute.Strength.ToString (), 4, 7, new RewardImpl.DoNothingReward("The Chicken looks kinda scary, and you back off.")),
-            new ExplorationCriteria (TraitFactory.Attribute.Strength.ToString (), 7, 10, new RewardImpl.AnimalReward (chicken)),
-            new ExplorationCriteria (TraitFactory.Attribute.Strength.ToString (), 10, int.MaxValue, new RewardImpl.AnimalReward (twoChickens))
+            new ExplorationCriteria (TraitFactory.Attribute.Fighting.ToString (), int.MinValue, 6, new RewardImpl.RandomAnimalPenalty()),
+            new ExplorationCriteria (TraitFactory.Attribute.Fighting.ToString (), 6, 10, new RewardImpl.DoNothingReward("The Chicken looks kinda scary, and you back off.")),
+            new ExplorationCriteria (TraitFactory.Attribute.Fighting.ToString (), 10, 20, new RewardImpl.AnimalReward (chicken)),
+            new ExplorationCriteria (TraitFactory.Attribute.Fighting.ToString (), 20, int.MaxValue, new RewardImpl.AnimalReward (twoChickens))
         };
 
         e.description = "You see a nest with a few eggs.  Its mother, a hugely oversized chicken, is nearby.";
         e.options = new List<ExplorationEvent.Option>() {
             new ExplorationEvent.Option("Fight the chicken", variableChickenReward),
-            new ExplorationEvent.Option("Run like the wind", TraitFactory.Attribute.Tracking.ToString(), 2, new List<AnimalDef>(), new List<string>()),
-            new ExplorationEvent.Option("Build a defensive wall", TraitFactory.Attribute.Strength.ToString(), 14, new List<AnimalDef>(), new List<string>())
+            new ExplorationEvent.Option("Run like the wind", TraitFactory.Attribute.Tracking.ToString(), 4, new List<AnimalDef>(), new List<string>()),
+            new ExplorationEvent.Option("Build a defensive wall", TraitFactory.Attribute.Strength.ToString(), 20, new List<AnimalDef>(), new List<string>())
         };
 
         return e;
